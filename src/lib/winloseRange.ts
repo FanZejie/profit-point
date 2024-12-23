b7243136012ec815515b4606f6e3526d76d7debd
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-12-23 20:56:52 +0800

    short range calculate
    The ETH 15-minute short-term trading calculator can help us quickly calculate the appropriate take-profit point and the amount of loss that reaches the stop loss based on the stop loss.

---
+++ src/lib/winloseRange.ts
@@ -0,0 +1,33 @@
+export default function calculateTrading(currentPrice: number, stopLossPrice: number, leverage: number, investment: number): { [key: string]: string } {
+    const isUpward: boolean = currentPrice > stopLossPrice; // 判断方向
+    const points = calculatePoints(currentPrice, stopLossPrice, isUpward);
+    const loss = calculateLoss(currentPrice, stopLossPrice, leverage, investment);
+    return  {
+        ...points,
+        loss
+    };
+}
+
+const calculatePoints = (currentPrice: number, stopLossPrice: number, isUpward: boolean): { [key: string]: string } => {
+    const lossRange: number = Math.abs(currentPrice - stopLossPrice); // 获取波动范围
+    const directionMultiplier: number = isUpward ? 1 : -1; // 根据方向选择加减
+
+    // 计算各止盈点
+    const winRange1_5: string = (currentPrice + directionMultiplier * lossRange * 1.5).toFixed(2);
+    const winRange2: string = (currentPrice + directionMultiplier * lossRange * 2).toFixed(2);
+    const winRange3: string = (currentPrice + directionMultiplier * lossRange * 3).toFixed(2);
+
+    return {
+        winRange1_5,
+        winRange2,
+        winRange3
+    };
+};
+
+// 计算亏损金额
+const calculateLoss = (currentPrice: number, stopLossPrice: number, leverage: number, investment: number): string => {
+    const lossRange: number = Math.abs(currentPrice - stopLossPrice); // 获取波动范围
+    const ethAmount: number = investment / currentPrice; // 计算买入的ETH数量
+    const lossAmount: number = lossRange * ethAmount * leverage; // 计算亏损金额
+    return lossAmount.toFixed(2); // 返回亏损金额，保留两位小数
+};
